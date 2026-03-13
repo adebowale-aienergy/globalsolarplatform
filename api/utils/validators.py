16227691e8c70e4fe59260cb545faf14440dbd24
Adebowale Immanuel Adeyemi <immanueladebowaleadeyemi@yahoo.com>
2026-03-13 23:44:09 +0100

    Implement validate_required_keys function
    Add a function to validate required keys in a payload

--- api/utils/validators.py
+++ api/utils/validators.py
@@ -1,1 +1,5 @@
+from typing import Iterable
 
+
+def validate_required_keys(payload: dict, required_keys: Iterable[str]) -> list[str]:
+    return [key for key in required_keys if key not in payload]
